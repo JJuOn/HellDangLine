89fbc0553b7c2967c4dea541e82e121341fd5f22
서주원 <jwseo001@khu.ac.kr>
2019-05-18 20:28:02 +0900

    Implement metrotimetable.py and Get timetables of each stations

--- metroLocation.py
+++ metroLocation.py
@@ -4,7 +4,7 @@ import xml.dom.minidom
 from env import *
 
 
-def getMetroLocation(END_INDEX,START_INDEX,rawSubwayNm):
+def getMetroLocation(END_INDEX, START_INDEX, rawSubwayNm):
     subwayNm = urllib.parse.quote_plus(rawSubwayNm)
     METRO_LOCATION_URL = "http://swopenapi.seoul.go.kr/api/subway/" + METRO_LOCATION_APIKEY + "/xml/realtimePosition/" + str(START_INDEX) + "/" + str(END_INDEX) + "/" + subwayNm
     # print(METRO_LOCATION_URL)
---
+++ metroTimetable.py
@@ -0,0 +1,54 @@
+import csv
+import urllib.request
+import urllib.parse
+import xml.dom.minidom
+import xml.etree.ElementTree as etree
+import json
+from time import sleep
+from env import *
+
+baseUrl = 'http://openAPI.seoul.go.kr:8088/'+METRO_TIMETABLE_APIKEY2+'/json/SearchSTNTimeTableByIDService/1/610'
+# 1: 평일, 2: 토요일, 3: 주말
+WEEK_TAG = ['1', '2', '3']
+# 1: 상행, 2: 하행
+INOUT_TAG = ['1', '2']
+lineNames = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'Suin', 'Bundang', 'SinBundang', 'GyeonguiJoungang', 'Airport', 'GyeongChun']
+
+
+def getMetroTimetable(lineName):
+    input = open('data/metroId/line'+lineName+'.csv', 'r')
+    lines = csv.reader(input)
+    for line in lines:
+        stationCode = line[3]
+        if len(stationCode) == 3:
+            stationCode = '0' + stationCode
+        for week in WEEK_TAG:
+            for inout in INOUT_TAG:
+                timetable = urllib.request.urlopen(baseUrl+'/'+stationCode+'/'+week+'/'+inout)
+                dictTimetable = json.load(timetable)
+                TRAIN_NOs = []
+                ORIGINSTATIONs = []
+                DESTSTATIONs = []
+                ARRIVETIMEs = []
+                EXPRESS_YNs = []
+                print(dictTimetable)
+                if not 'SearchSTNTimeTableByIDService' in dictTimetable:
+                    continue
+                for row in dictTimetable['SearchSTNTimeTableByIDService']['row']:
+                    TRAIN_NOs.append(row['TRAIN_NO'])
+                    ORIGINSTATIONs.append(row['ORIGINSTATION'])
+                    DESTSTATIONs.append(row['DESTSTATION'])
+                    ARRIVETIMEs.append(row['ARRIVETIME'])
+                    EXPRESS_YNs.append(row['EXPRESS_YN'])
+                output = open('data/timetable/' + line[0] + '_' + line[3] + '_' + week + '_' + inout + '.csv', 'w', encoding='euc-kr', newline='')
+                outputWriter = csv.writer(output)
+                outputWriter.writerow(['TRAIN_NO', 'ORIGINSTATION', 'DESTSTATION', 'ARRIVETIME', 'EXPRESS_YN'])
+                for i in range(0, len(TRAIN_NOs)):
+                    outputWriter.writerow([TRAIN_NOs[i], ORIGINSTATIONs[i], DESTSTATIONs[i], ARRIVETIMEs[i], EXPRESS_YNs[i]])
+                output.close()
+    input.close()
+
+
+for lineName in lineNames:
+    getMetroTimetable(lineName)
+
