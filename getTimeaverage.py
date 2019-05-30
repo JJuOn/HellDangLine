cf1dd1798b4793bf622623731d09d4cb33208c50
서주원 <jwseo001@khu.ac.kr>
2019-05-30 12:38:46 +0900

    Implement getTimeaverage.py

--- getTimeaverage.py
+++ getTimeaverage.py
@@ -2,3 +2,23 @@ import csv
 import os
 
 
+def getTimeaverage():
+    targetFileList = os.listdir('data/timesum')
+    for targetFile in targetFileList:
+        print('processing : '+targetFile)
+        inputFile = open('data/timesum/'+targetFile, 'r')
+        lines = csv.reader(inputFile)
+        temp = []
+        for line in lines:
+            temp.append([line[0], line[1], int(line[2])//int(line[3])])
+        inputFile.close()
+        if not os.path.exists('data/timeaverage'):
+            os.mkdir('data/timeaverage')
+        outputFile = open('data/timeaverage/'+targetFile, 'w', encoding='euc-kr', newline='')
+        csvWriter = csv.writer(outputFile)
+        for row in temp:
+            csvWriter.writerow(row)
+        outputFile.close()
+
+
+getTimeaverage()
