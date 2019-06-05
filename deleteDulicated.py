f3c189f81f9729173dee4101d34169dd22d57a76
서주원 <jwseo001@khu.ac.kr>
2019-06-06 02:50:58 +0900

    Remove duplicated data 20190605

--- deleteDulicated.py
+++ deleteDulicated.py
@@ -3,7 +3,7 @@ import csv
 
 
 def deleteDuplicated():
-    targetDate = '20190604'
+    targetDate = '20190605'
     fileNameList = os.listdir('data/location/{}'.format(targetDate))
     for fileName in fileNameList:
         print(fileName)
