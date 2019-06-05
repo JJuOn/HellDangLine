faf6e821ba2406d72ca04b3ebfb36e6a590e1bff
서주원 <jwseo001@khu.ac.kr>
2019-06-05 19:30:43 +0900

    Remove duplicated data 20190604

---
+++ deleteDulicated.py
@@ -0,0 +1,26 @@
+import os
+import csv
+
+
+def deleteDuplicated():
+    targetDate = '20190604'
+    fileNameList = os.listdir('data/location/{}'.format(targetDate))
+    for fileName in fileNameList:
+        print(fileName)
+        original = open('data/location/{}/{}'.format(targetDate, fileName), 'r')
+        lines = csv.reader(original)
+        temp = []
+        for line in lines:
+            if not line in temp:
+                temp.append(line)
+            else:
+                print('found duplicated')
+        original.close()
+        output = open('data/location/{}/{}'.format(targetDate, fileName), 'w', encoding='euc-kr', newline='')
+        csvWriter = csv.writer(output)
+        for row in temp:
+            csvWriter.writerow(row)
+        output.close()
+
+
+deleteDuplicated()
